50d688effe53911229872cae09ffa9e4cf055b18
CapisizuCosmin <cosmineala@gmail.com>
2021-03-12 22:38:15 +0200

    Homework 1 V1.0

--- MCapp/app/src/main/java/com/example/mcapp/MainActivity.kt
+++ MCapp/app/src/main/java/com/example/mcapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mcapp
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) // TODO -> Disables screen Totation
 
         setupActionBarWithNavController( findNavController(R.id.fragment) )
     }
--- MCapp/app/src/main/java/com/example/mcapp/fragments/SensorsFragment.kt
+++ MCapp/app/src/main/java/com/example/mcapp/fragments/SensorsFragment.kt
@@ -2,17 +2,22 @@ package com.example.mcapp.fragments
 
 import android.content.Context
 import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.ContextCompat.getSystemService
 import com.example.mcapp.R
+import kotlinx.android.synthetic.main.fragment_sensors.*
+import kotlin.math.roundToInt
 
 
-class SensorsFragment : Fragment() {
+class SensorsFragment : Fragment(), SensorEventListener {
 
 //    private lateinit var sensorManager: SensorManager
 //    private var sensor: Sensor? = null
@@ -27,9 +32,40 @@ class SensorsFragment : Fragment() {
         val sensorManager = view.context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
         val sensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
 
+        sensorManager.registerListener( this, sensor, 10000 )
+
 
 
         return view
     }
 
+    override fun onSensorChanged(event: SensorEvent?) {
+
+        if (event != null) {
+
+            var v1: Float =  event.values[0]
+            var v2: Float =  event.values[1]
+            var v3: Float =  event.values[2]
+
+            v1 = v1 * 10 / 2 + 50
+            v2 = v2 * 10 / 2 + 50
+            v3 = v3 * 10 / 2 + 50
+
+            val pro1 : Int = v1.toInt()
+            val pro2 : Int = v2.toInt()
+            val pro3 : Int = v3.toInt()
+
+            val textval = pro1.toString() + " : " + pro2 + " : " + pro3
+            tvinfo.text = textval
+
+            pb1.progress = pro1
+            pb2.progress = pro2
+            pb3.progress = pro3
+
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
 }
