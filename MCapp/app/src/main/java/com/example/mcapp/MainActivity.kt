3f87c65755cef7b4d3a9b93be6a2993500ba2578
CapisizuCosmin <cosmineala@gmail.com>
2021-03-12 18:39:40 +0200

    Added views and navigation for Sensor Fragment
    added ListFragment, the root from where you can navigate to others fragment
    added SensorsFragment, the fragment that uses gyroscope to represent the device rotation

--- MCapp/app/src/main/java/com/example/mcapp/MainActivity.kt
+++ MCapp/app/src/main/java/com/example/mcapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.mcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController( findNavController(R.id.fragment) )
     }
+
+
 }
---
+++ MCapp/app/src/main/java/com/example/mcapp/fragments/ListFragment.kt
@@ -0,0 +1,33 @@
+package com.example.mcapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.mcapp.R
+import kotlinx.android.synthetic.main.fragment_list.view.*
+
+
+class ListFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        view.sensor_icon.setOnClickListener {
+            findNavController().navigate( R.id.action_listFragment_to_sensorsFragment2 )
+        }
+
+
+        // Inflate the layout for this fragment
+        return view
+    }
+
+
+}
