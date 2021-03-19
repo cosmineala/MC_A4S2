e9cd2351c5746484196d3336e5795260735e9c3e
CapisizuCosmin <cosmineala@gmail.com>
2021-03-19 17:55:16 +0200

    Update: Sensors Fragment

--- MCapp/app/src/main/java/com/example/mcapp/Sensors/SensorsFragment.kt
+++ MCapp/app/src/main/java/com/example/mcapp/Sensors/SensorsFragment.kt
@@ -36,35 +36,11 @@ class SensorsFragment : Fragment(){
         sensorManager = view.context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
 
-        sensorEventListener = object : SensorEventListener{
-            override fun onSensorChanged(event: SensorEvent?) {
-                if (event != null) {
-                    var v1: Float =  event.values[0]
-                    var v2: Float =  event.values[1]
-                    var v3: Float =  event.values[2]
 
-                    v1 = v1 * 10 / 2 + 50
-                    v2 = v2 * 10 / 2 + 50
-                    v3 = v3 * 10 / 2 + 50
-
-                    val pro1 : Int = v1.toInt()
-                    val pro2 : Int = v2.toInt()
-                    val pro3 : Int = v3.toInt()
-
-                    val textval = pro1.toString() + " : " + pro2 + " : " + pro3
-                    tvinfo.text = textval
-
-                    pb1.progress = pro1
-                    pb2.progress = pro2
-                    pb3.progress = pro3
-                }
-            }
-            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-                // NOTE Empty but requared method
-            }
-        }
-
+        sensorEventListener = sensorEventListenerCode
         sensorManager.registerListener( sensorEventListener, sensor, 10000 )
+
+
 
         return view
     }
@@ -74,6 +50,34 @@ class SensorsFragment : Fragment(){
         sensorManager.unregisterListener( sensorEventListener, sensor )
         super.onStop()
 
+    }
+
+    private var sensorEventListenerCode = object : SensorEventListener{
+        override fun onSensorChanged(event: SensorEvent?) {
+            if (event != null) {
+                var v1: Float =  event.values[0]
+                var v2: Float =  event.values[1]
+                var v3: Float =  event.values[2]
+
+                v1 = v1 * 10 / 2 + 50
+                v2 = v2 * 10 / 2 + 50
+                v3 = v3 * 10 / 2 + 50
+
+                val pro1 : Int = v1.toInt()
+                val pro2 : Int = v2.toInt()
+                val pro3 : Int = v3.toInt()
+
+                val textval = pro1.toString() + " : " + pro2 + " : " + pro3
+                tvinfo.text = textval
+
+                pb1.progress = pro1
+                pb2.progress = pro2
+                pb3.progress = pro3
+            }
+        }
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+            // NOTE Empty but requared method
+        }
     }
 
 }
