9b2ededcd507e2736c581f0aced616aaf0e0ed8b
CapisizuCosmin <cosmineala@gmail.com>
2021-03-23 14:50:29 +0200

    (Stable) Added: Camera Fragment; Update: Camera permissions Fragment
    Added: Camera preview.

--- MCapp/app/src/main/java/com/example/mcapp/MainActivity.kt
+++ MCapp/app/src/main/java/com/example/mcapp/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.mcapp
 
+import android.content.Context
 import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +23,18 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.fragment)
         navController.navigateUp()
         return super.onSupportNavigateUp()
+    }
+
+    companion object {
+
+        /** Use external media if it is available, our app's file directory otherwise */
+        fun getOutputDirectory(context: Context): File {
+            val appContext = context.applicationContext
+            val mediaDir = context.externalMediaDirs.firstOrNull()?.let {
+                File(it, appContext.resources.getString(R.string.app_name)).apply { mkdirs() } }
+            return if (mediaDir != null && mediaDir.exists())
+                mediaDir else appContext.filesDir
+        }
     }
 
 
--- MCapp/app/src/main/java/com/example/mcapp/MyCameraX/CameraPermissionsFragment.kt
+++ MCapp/app/src/main/java/com/example/mcapp/MyCameraX/CameraPermissionsFragment.kt
@@ -1,18 +1,61 @@
 package com.example.mcapp.MyCameraX
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.mcapp.R
 
+private const val PERMISSIONS_REQUEST_CODE = 10
+private val PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.CAMERA)
 
 class CameraPermissionsFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        if (!hasPermissions(requireContext())) {
+            // Request camera-related permissions
+            requestPermissions(PERMISSIONS_REQUIRED, PERMISSIONS_REQUEST_CODE)
+        } else {
+            // If permissions have already been granted, proceed
+            navigateToCamera()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSIONS_REQUEST_CODE) {
+            if (PackageManager.PERMISSION_GRANTED == grantResults.firstOrNull()) {
+                // Take the user to the success fragment when permission is granted
+                Toast.makeText(context, "Permission request granted", Toast.LENGTH_LONG).show()
+                navigateToCamera()
+            } else {
+                Toast.makeText(context, "Permission request denied", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun navigateToCamera() {
+       findNavController().navigate(R.id.action_cameraPermissionsFragment_to_cameraFragment)
+    }
+
+    companion object {
+
+        /** Convenience method used to check if all permissions required by this app are granted */
+        fun hasPermissions(context: Context) = PERMISSIONS_REQUIRED.all {
+            ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
